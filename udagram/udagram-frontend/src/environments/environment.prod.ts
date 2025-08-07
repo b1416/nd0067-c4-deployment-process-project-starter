6ed63f274739326131d01a50cc11c699ceea617a
bmulla <bmulla@elm.sa>
2025-08-07 14:20:15 +0300

    update prod env in frontend

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'dagram',
-  apiHost: 'http://localhost:8080/api/v0',
+  apiHost: 'http://udagram-api-dev.us-west-2.elasticbeanstalk.com/api/v0',
 };
 
 /*
